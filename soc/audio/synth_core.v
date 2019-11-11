9ed334ef0c5603db9163bec29dee62613bd67400
Alan Green <alan.green@gmail.com>
2019-11-12 06:56:04 +1100

    sorting stuff out

--- soc/audio/synth_core.v
+++ soc/audio/synth_core.v
@@ -75,6 +75,7 @@ module synth_core #(
 );
 
 	localparam PHASE_WIDTH = PHASE_INT + PHASE_FRAC;
+	parameter integer M32_DIV_WIDTH = 32 - DIV_WIDTH;
 
 
 	// Signals
@@ -198,7 +199,7 @@ module synth_core #(
 
 	always @(*)
 		case (crb_reg)
-			3'h0:    crb_data = { {(32-DIV_WIDTH){1'b0}}, cfg_div };
+			3'h0:    crb_data = { {M32_DIV_WIDTH{1'b0}}, cfg_div };
 			3'h1:    crb_data = { 24'h000000, cfg_vol };
 			3'h2:    crb_data = { 16'h0000, cfg_voice_force };
 			3'h3:    crb_data = { 16'h0000, cfg_voice_start };
